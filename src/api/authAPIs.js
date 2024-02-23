06289bc47eaa947b1e34cf6a5b48554c9548fdce
chamara-madhu <nlc.madhushanka@gmail.com>
2024-02-23 11:16:16 +0530

    changing env

--- src/api/authAPIs.js
+++ src/api/authAPIs.js
@@ -1,19 +1,14 @@
 import axios from "axios";
+import config from "../config.js/api";
 
 export const loginAPI = (data) => {
-  return axios.post(`${process.env.REACT_APP_API_URL}/v1/api/auth/login`, data);
+  return axios.post(`${config.API_URL}/v1/api/auth/login`, data);
 };
 
 export const verifyOtpAPI = (data) => {
-  return axios.post(
-    `${process.env.REACT_APP_API_URL}/v1/api/auth/verify-otp`,
-    data
-  );
+  return axios.post(`${config.API_URL}/v1/api/auth/verify-otp`, data);
 };
 
 export const signUpAPI = (data) => {
-  return axios.post(
-    `${process.env.REACT_APP_API_URL}/v1/api/auth/sign-up`,
-    data
-  );
+  return axios.post(`${config.API_URL}/v1/api/auth/sign-up`, data);
 };
