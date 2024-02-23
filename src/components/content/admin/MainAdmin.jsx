c9dfd8ff61e544448e814e2fbbc0cedaaaca780e
udara <uvlakshan@gmail.com>
2024-02-23 23:17:58 +0530

    Added admin page modifications

--- src/components/content/admin/MainAdmin.jsx
+++ src/components/content/admin/MainAdmin.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import NewRequests from "./NewRequests/NewRequests";
 import AllApplications from "./AllApplications/AllApplications";
-
 import SeasonTicketsImage from "../../../assets/images/SeasonTicketsImage.jpg";
 import Payments from "./Payments/Payments";
+import SendImage from "../../../assets/images/send.png";
+import ArchiveImage from "../../../assets/images/archive.png";
+import PaymentImage from "../../../assets/images/paymentImage.png";
 
 const MainAdmin = () => {
-  const [selectedContent, setSelectedContent] = useState("seasonTickets");
+  const [selectedContent, setSelectedContent] = useState("newRequests");
   return (
     <div className="flex w-full">
     <div
@@ -14,15 +16,15 @@ const MainAdmin = () => {
       style={{ height: "calc(100vh - 72px)" }}
     >
       <div className="flex items-center mb-2 cursor-pointer" onClick={() => setSelectedContent("newRequests")}>
-        <img  style={{ width: '30px', height: 'auto', marginLeft: '12px' }} alt="S icon" className="mr-2" />
+        <img  src= {SendImage} style={{ width: '25px', height: 'auto', marginLeft: '35px' }} alt="N icon" className="mr-2" />
         <span className={`font-bold ${selectedContent === "newRequests" ? "text-blue-500" : ""}`}>New Requests</span>
       </div>
       <div className="flex items-center mb-2 cursor-pointer" onClick={() => setSelectedContent("allapplications")}>
-        <img style={{ width: '30px', height: 'auto', marginLeft: '12px' }} alt="B icon" className="mr-2" />
+        <img src={ArchiveImage} style={{ width: '22px', height: 'auto', marginLeft: '35px' }} alt="A icon" className="mr-2" />
         <span className={`font-bold ${selectedContent === "allapplications" ? "text-blue-500" : ""}`}>All Applications</span>
       </div>
       <div className="flex items-center mb-2 cursor-pointer" onClick={() => setSelectedContent("payments")}>
-        <img style={{ width: '30px', height: 'auto', marginLeft: '12px' }} alt="B icon" className="mr-2" />
+        <img src={PaymentImage} style={{ width: '22px', height: 'auto', marginLeft: '35px' }} alt="P icon" className="mr-2" />
         <span className={`font-bold ${selectedContent === "payments" ? "text-blue-500" : ""}`}>Payments</span>
       </div>
     </div>
--- src/components/content/admin/NewRequests/NewRequests.jsx
+++ src/components/content/admin/NewRequests/NewRequests.jsx
@@ -5,7 +5,7 @@ const NewRequests = () => {
 
   return (
     <div className="flex flex-col gap-4" style={{ backgroundColor: "rgba(255, 255, 255, 0.9)", padding: "20px" }}>
-      <h2 className="text-3xl font-bold">New Requests</h2>
+      <h2 className="text-xl font-bold">New Requests</h2>
     </div>
   );
 };
