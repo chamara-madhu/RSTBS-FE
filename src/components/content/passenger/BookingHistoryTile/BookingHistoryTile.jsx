cb664454cdb896e1418ab1f3c38e3dceeb5a1468
udara <uvlakshan@gmail.com>
2024-02-23 19:24:57 +0530

    Fixed CSS issues

--- src/components/content/passenger/BookingHistoryTile/BookingHistoryTile.jsx
+++ src/components/content/passenger/BookingHistoryTile/BookingHistoryTile.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const BookingHistoryTile = () => {
   const bookingData = [
     {
-      startDate: '01/01/2024',
-      endDate: '01/02/2024',
-      isActive: false,
+      startDate: '02/02/2024',
+      endDate: '02/03/2024',
+      isActive: true,
       stationOrigin: 'Anuradhapura',
       destination: 'Maradana',
       daysLeft: 38
     },
     {
-      startDate: '02/02/2024',
-      endDate: '02/03/2024',
-      isActive: true,
+      startDate: '01/01/2024',
+      endDate: '01/02/2024',
+      isActive: false,
       stationOrigin: 'Anuradhapura',
       destination: 'Maradana',
       daysLeft: 10
@@ -24,15 +24,24 @@ const BookingHistoryTile = () => {
     <div className="flex flex-col gap-4" style={{ backgroundColor: "rgba(255, 255, 255, 0.9)", padding: "20px" }}>
       <h2 className="text-3xl font-bold">Your Booking History</h2>
       {bookingData.map((booking, index) => (
-        <div key={index} className="mt-4 border-2 border-blue-500 p-4"> {/* Add border and padding */}
+        <div key={index} className="mt-4 border-4 border-blue-800 p-4 ">
           {index > 0}
-          <p className="font-bold">Current Season Ticket Details</p>
-          <p>{`${booking.startDate} - ${booking.endDate}`} <span className={booking.isActive ? 'text-green-500' : 'text-red-500'}>{booking.isActive ? 'Active' : 'Inactive'}</span></p>
-          <hr className="my-2 border-t-2 border-gray-400" /> {/* Horizontal line to separate sections within a block */}
-          <p className="font-bold">Your Season Ticket</p>
-          <p>Station Origin: {booking.stationOrigin}</p>
-          <p>Destination: {booking.destination}</p>
-          <p>Days Left: {booking.daysLeft} Days</p>
+          <p className="text-lg font-bold mb-4">Season Ticket Details</p>
+          <p className="flex items-center">
+            <span className="text-lg font-medium mb-2">{`${booking.startDate} - ${booking.endDate}`}</span>
+            <span className="ml-4">
+                <button className={`text-white font-bold py-2 px-4 rounded-full ${booking.isActive ? 'bg-green-500' : 'bg-red-500'}`}>
+                {booking.isActive ? 'Active' : 'Inactive'}
+                </button>
+            </span>
+           </p>
+          <hr className="my-2 border-t-2 border-gray-400" />
+          
+        <p style={{ whiteSpace: 'nowrap' }}>
+        <span style={{ display: 'inline-block', width: '250px' }}><span className="font-bold">Station Origin:</span> {booking.stationOrigin}</span>
+        <span style={{ display: 'inline-block', width: '230px' }}><span className="font-bold">Destination:</span> {booking.destination}</span>
+        <span style={{ display: 'inline-block', width: '230px' }}><span className="font-bold">Days Left:</span> {booking.daysLeft} Days</span>
+        </p>
         </div>
       ))}
     </div>
