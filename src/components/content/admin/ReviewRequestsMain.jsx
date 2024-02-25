f958b3d6eeb63d953ebe469105c563cdbc241065
udara <uvlakshan@gmail.com>
2024-02-25 11:28:21 +0530

    Added few changes in ReviewRequests UI

--- src/components/content/admin/ReviewRequestsMain.jsx
+++ src/components/content/admin/ReviewRequestsMain.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import BackArrowIcon from "./../../../assets/icons/back-arrow-3095.svg";
+import Button from '../../shared/buttons/Button';
 
 const ReviewRequestsMain = () => {
   const location = useLocation();
   const selectedRequest = location.state?.selectedRequest;
 
+  console.log(selectedRequest);
+
   return (
     <div className="flex flex-col gap-4" style={{ backgroundColor: "rgba(255, 255, 255, 0.9)", padding: "20px" }}>
       <div className="flex items-center">
         <Link to="/admin/new-requests" className="mr-2">
-          <img src={BackArrowIcon} alt="BackArrow" style={{ width: "30px", height: "30px" }}/>
+          <img src={BackArrowIcon} alt="BackArrow" style={{ width: "30px", height: "30px" }} />
         </Link>
         <h2 className="text-xl font-bold">Review Requests</h2>
       </div>
@@ -20,22 +23,54 @@ const ReviewRequestsMain = () => {
           <table className="table-fixed">
             <thead>
               <tr>
-                <th className="px-4 py-2">Element Name</th>
-                <th className="px-4 py-2">Value</th>
+                <th className="px-4 py-2 text-left">Element Name</th>
+                <th className="px-4 py-2 text-left">Value</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="px-4 py-2">Full Name</td>
-                <td className="px-4 py-2">{selectedRequest.applicationId.fullName}</td>
+                <td className="px-4 py-2 text-left">Full Name</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.applicationId.fullName}</td>
               </tr>
               <tr>
-                <td className="px-4 py-2">NIC</td>
-                <td className="px-4 py-2">{selectedRequest.applicationId.nic}</td>
+                <td className="px-4 py-2 text-left">NIC</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.applicationId.nic}</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2 text-left">Station Origin</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.applicationId.stations.origin}</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2 text-left">Station Destination</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.applicationId.stations.destination}</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2 text-left">Duration</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.applicationId.duration}</td>
+              </tr>
+              <tr>
+                <td className="px-4 py-2 text-left">Amount</td>
+                <td className="px-4 py-2 text-left">{selectedRequest.amount}</td>
               </tr>
             </tbody>
           </table>
         )}
+      </div>
+      <div className="flex flex-row gap-6 mt-4">
+        <Button
+          type="submit"
+          variant="primary"
+          className="w-[200px]"
+        // isLoading={loading}
+        >
+          Accept Request
+        </Button>
+        <Button type="submit" variant="dark"
+          // isLoading={loading}
+          className="w-[200px]"
+        >
+          Reject Request
+        </Button>
       </div>
     </div>
   );
